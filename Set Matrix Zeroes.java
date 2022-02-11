a6dfb898ee2820594e6f059b5970bfbed9ea4fe2
ashuanand1995 <bunty.ashutosh.anand@gmail.com>
2022-02-11 13:17:24 +0530

    Create Set Matrix Zeroes.java

---
+++ Set Matrix Zeroes.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean firstCol = false;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        for(int i=0;i<row;i++){
+            if(matrix[i][0]==0){
+                   firstCol=true; 
+                }
+            
+            for(int j=1;j<col;j++){                
+                
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        
+        if(matrix[0][0]==0){
+            for(int j=1;j<col;j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(firstCol){
+            for(int i=0;i<row;i++){
+                matrix[i][0]=0;
+            }
+        }
+    }
+}
