b6ed47261cc93a93958e7cc5c995d31e0d478544
ashuanand1995 <bunty.ashutosh.anand@gmail.com>
2022-09-05 17:52:41 +0530

    Time: 1 ms (27.92%), Space: 42.6 MB (35.29%) - LeetHub

---
+++ 66-plus-one/66-plus-one.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for(int i=n-1;i>=0;i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        int[] newRes = new int[n+1];
+        newRes[0] = 1;
+        return newRes;
+    }
+}
