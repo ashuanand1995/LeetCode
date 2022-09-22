c05c99704ef22280867b1549093c0307637dc4da
ashuanand1995 <bunty.ashutosh.anand@gmail.com>
2022-09-22 23:39:55 +0530

    Time: 2 ms (91.23%), Space: 42.4 MB (23.31%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,34 +1,16 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stck = new Stack<Character>();
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            char rev = getReversedBracket(c);
-            if(isLeftParentheses(c)){
-                stck.push(c);
-            }else if(stck.isEmpty() || !(stck.pop()==rev)) {
-                return false;
-                }
-            }
-        return stck.isEmpty();    
-    }
-    
-    
-    
-    public boolean isLeftParentheses(char c){
-        if(c=='['||c=='{'||c=='('){
-            return true;
-        }
-        return false;
-    }
-    
-    public char getReversedBracket(char c){
-        if(c=='[') return ']';
-        if(c=='{') return '}';
-        if(c=='(') return ')';
-        if(c==']') return '[';
-        if(c=='}') return '{';
-        if(c==')') return '(';
-        return ' ';
+        Stack<Character> stack = new Stack<Character>();
+	for (char c : s.toCharArray()) {
+		if (c == '(')
+			stack.push(')');
+		else if (c == '{')
+			stack.push('}');
+		else if (c == '[')
+			stack.push(']');
+		else if (stack.isEmpty() || stack.pop() != c)
+			return false;
+	}
+	return stack.isEmpty();
     }
 }
